bf4a6bc349cec02480202af6ae206035b7f9a0ec
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-11-06 18:26:18 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const usersRoutes = require('./routes/usersRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
